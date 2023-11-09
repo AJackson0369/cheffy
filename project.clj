b4f827ae8c58eb0eba8f8c69e3fe814f2ee2dca5
Jackie Whittle <jwhittle@koboldhunters.com>
2023-11-09 13:35:22 -0600

    Create Heroku

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [ovotech/ring-jwt "2.3.0"]]
   :profiles {:uberjar {:aot :all}
              :dev {:source-paths ["dev/src"]
-                   :resource-paths ["dev/resources"]
+                   :resource-paths ["dev/dev-resources"]
                    :dependencies [[ring-mock "0.1.5"]
                                   [integrant/repl "0.3.3"]]}}
   :uberjar-name "cheffy.jar")
